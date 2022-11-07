02352eaa09bfc9d6732a034bd4d2ef8be5d4d020
MajaLHJensen <113036520+MajaLHJensen@users.noreply.github.com>
2022-11-07 13:06:16 +0100

    heje
    euwf

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,25 @@ public class Main
 {
     public static void main(String[] args)
     {
+<<<<<<< Updated upstream
+=======
+        System.out.println("Hello world!");
+
+        /*ArrayList<Media>medias = new ArrayList<>();
+        medias.add(new Movies());
+        Media movie1 = new Movies();
+*/
+        User.pickMedia(null);
+    }
+
+
+
+    // TODO: 03-11-2022 Lav constructor i Media, så Media kan returneres i metoden savedList
+    //public static Media savedList()
+    public static void savedList()
+    {
+
+>>>>>>> Stashed changes
         StartMenu.startStreaming();
     }
 
