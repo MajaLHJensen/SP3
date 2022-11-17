cf6b08808c809e377481e08c16c4cff1ae3729a4
elsaD01 <cph-ed94@cphbusiness.dk>
2022-11-17 11:11:03 +0100

    Made a TextUI and cleaned up Text/FileIO

--- src/FileIO.java
+++ src/FileIO.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class FileIO{
 
     private Media media;
-
     protected final Scanner scanner = new Scanner(System.in);
+    ArrayList<User> users = new ArrayList<>();
     ArrayList<Media> movies = new ArrayList<>();
-
 
     // Arraylist of Media and choose Movie
     public ArrayList<Media> readMovieData(){
@@ -47,6 +46,81 @@ public class FileIO{
             System.out.println(e + "Option do not exist.Try again");
         }
         return movies;
+    }
+
+    public void register(String userName, int userPassword){
+
+        //Transfer new login to the file(UserLogin.csv) when the user signs up.
+        try{
+            File file = new File("data/UserLogin.csv");
+            if (!file.exists()){
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.newLine();
+            String userLogin = userName + ", " + userPassword;
+            bw.write(userLogin);
+            bw.close();
+            User newUser = new User(userName, userPassword);
+            users.add(newUser);
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public boolean checkUser(String name, int password){
+        try{
+            File file = new File("data/UserLogin.csv");
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] user = line.split(", ");
+                String userNameFromFile = user[0];
+                int passwordFromFile = Integer.parseInt(user[1]);
+
+                if (name.equals(userNameFromFile))
+                    if (password == passwordFromFile)
+                        return true;
+                System.out.println(name);
+                System.out.println("Username does exist");
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println("UserLogin.csv does not exist");
+            System.out.println("No users found, please register:");
+            String userName = scanner.nextLine();
+            int userPassword = scanner.nextInt();
+            register(userName, userPassword);
+            e.printStackTrace();
+        }
+        scanner.close();
+        return false;
+    }
+
+    // check user is already registered on UserLogin.csv when they are trying to create a new account
+    public boolean checkUserSignUp(String userName){
+        try{
+            File file = new File("data/UserLogin.csv");
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] user = line.split(", ");
+                String userNameFromFile = user[0];
+
+                if (userName.equals(userNameFromFile))
+                    return false;
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println("UserLogin.csv does not exist");
+            userName = scanner.nextLine();
+            e.printStackTrace();
+        }
+        return true;
     }
 }
 
